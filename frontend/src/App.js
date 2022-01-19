f8d1856ecdc0ce552e7a579b9e7e731ebdcc65b3
Anant Shukla <anantshukla1402@gmail.com>
2022-01-19 18:06:46 +0530

    Installed react bootstrap
    -npm installed react bootstrap
    -dashboard boilerplate code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 //import TestComponent from "./components/TestComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginSignup from "./components/LoginSignup/LoginSignup";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            
+          </Col>
+          <Col>
+            
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
